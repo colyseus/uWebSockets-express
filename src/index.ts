baa93265b0101125a06a4d11ff464cf1b87fc3b7
Levy Barbosa <marcio.levy20@gmail.com>
2023-01-28 11:02:46 -0300

    re-exporting types in index.ts - added lib/ to .gitignore - updated package-lock.json to match version in package.json

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,8 @@ import { Application } from "./Application";
 export default function (app: uWS.TemplatedApp) {
   return new Application(app);
 }
+
+export { Application };
+export { IncomingMessage } from "./IncomingMessage";
+export { ServerResponse } from "./ServerResponse";
+export { Socket } from "./Socket";

84ea5761a74331c4768419e9f75961904ae2aaa8
Endel Dreyer <endel.dreyer@gmail.com>
2023-06-29 09:54:41 -0300

    add simple async example

---
+++ example/async_test.ts
@@ -0,0 +1,20 @@
+import uWS from "uWebSockets.js";
+import express from "express";
+import expressify from "../src";
+
+// import promisified timers
+import { setTimeout } from "timers/promises";
+
+const PORT = 8080;
+const app = expressify(uWS.App());
+
+app.get('/', async (req, res) => {
+  res.header("Cache-Control", "no-cache, no-store, must-revalidate");
+  await setTimeout(1000);
+
+  res.header("Content-Type", "application/json");
+  res.write(JSON.stringify({ hello: "world" }));
+  res.end();
+});
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
